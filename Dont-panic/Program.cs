6961572b1720b784761f78378dae46a94f8489a2
Appius <appius@outlook.com>
2017-05-12 10:48:15 +0300

    added War game solution

---
+++ Dont-panic/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace Dont_panic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int nbFloors = int.Parse(inputs[0]); // number of floors
+            int width = int.Parse(inputs[1]); // width of the area
+
+            var area = new Type[width, nbFloors];
+
+            int exitFloor = int.Parse(inputs[3]); // floor on which the exit is found
+            int exitPos = int.Parse(inputs[4]); // position of the exit on its floor
+            area[exitPos, exitFloor] = Type.Exit;
+
+            int nbElevators = int.Parse(inputs[7]); // number of elevators
+
+            for (int i = 0; i < nbElevators; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int elevatorFloor = int.Parse(inputs[0]); // floor on which this elevator is found
+                int elevatorPos = int.Parse(inputs[1]); // position of the elevator on its floor
+                area[elevatorPos, elevatorFloor] = Type.Elevator;
+            }
+
+            var blockedFloors = new HashSet<int>();
+
+            // game loop
+            while (true)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int cloneFloor = int.Parse(inputs[0]); // floor of the leading clone
+                int clonePos = int.Parse(inputs[1]); // position of the leading clone on its floor
+                Direction direction = (Direction) Enum.Parse(typeof(Direction), inputs[2]);
+
+                var targetPos = cloneFloor == exitFloor ? exitPos : FindElevator(area, cloneFloor);
+                if (blockedFloors.Contains(cloneFloor))
+                {
+                    Console.WriteLine("WAIT");
+                    continue;
+                }
+
+                if ((direction == Direction.LEFT && targetPos > clonePos) ||
+                    (direction == Direction.RIGHT && targetPos < clonePos))
+                {
+                    blockedFloors.Add(cloneFloor);
+                    Console.WriteLine("BLOCK");
+                    continue;
+                }
+
+                Console.WriteLine("WAIT");
+            }
+        }
+
+        private static int FindElevator(Type[,] area, int floor)
+        {
+
+            for (int i = 0; i < area.GetLength(1); i++)
+                if (area[i, floor] == Type.Elevator)
+                    return i;
+
+            return -1;
+        }
+
+        enum Type
+        {
+            None,
+            Elevator,
+            Exit
+        }
+
+        enum Direction
+        {
+            NONE,
+            LEFT,
+            RIGHT
+        }
+    }
+}
---
+++ War/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace War
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()); // the number of cards for player 1
+            var player1 = new Queue<int>(n);
+            for (var i = 0; i < n; i++)
+            {
+                var cardp1 = Console.ReadLine(); // the n cards of player 1
+                cardp1 = cardp1.Remove(cardp1.Length - 1);
+
+                int value;
+                if (!(int.TryParse(cardp1, out value)))
+                    value = ParseCard(cardp1);
+
+                player1.Enqueue(value);
+            }
+
+            var m = int.Parse(Console.ReadLine()); // the number of cards for player 2
+            var player2 = new Queue<int>();
+            for (var i = 0; i < m; i++)
+            {
+                var cardp2 = Console.ReadLine(); // the m cards of player 2
+                cardp2 = cardp2.Remove(cardp2.Length - 1);
+
+                int value;
+                if (!(int.TryParse(cardp2, out value)))
+                    value = ParseCard(cardp2);
+
+                player2.Enqueue(value);
+            }
+
+            var steps = 0;
+            while (player1.Count > 0 && player2.Count > 0)
+            {
+                var tempQueue1 = new Queue<int>(n);
+                var tempQueue2 = new Queue<int>(m);
+
+                var card1 = player1.Dequeue();
+                var card2 = player2.Dequeue();
+
+                steps++;
+                while (card1 == card2)
+                {
+                    tempQueue1.Enqueue(card1);
+                    tempQueue2.Enqueue(card2);
+
+                    if (player1.Count < 4 || player2.Count < 4)
+                    {
+                        Console.WriteLine("PAT");
+                        return;
+                    }
+
+                    for (var i = 0; i < 3; i++)
+                    {
+                        tempQueue1.Enqueue(player1.Dequeue());
+                        tempQueue2.Enqueue(player2.Dequeue());
+                    }
+
+                    card1 = player1.Dequeue();
+                    card2 = player2.Dequeue();
+                }
+
+                var winner = card1 > card2 ? player1 : player2;
+                while (tempQueue1.Count > 0)
+                    winner.Enqueue(tempQueue1.Dequeue());
+                winner.Enqueue(card1);
+
+                while (tempQueue2.Count > 0)
+                    winner.Enqueue(tempQueue2.Dequeue());
+                winner.Enqueue(card2);
+            }
+
+            var winnerNum = player1.Count > 0 ? 1 : 2;
+            Console.WriteLine($"{winnerNum} {steps}");
+        }
+
+        private static int ParseCard(string card)
+        {
+            switch (card)
+            {
+                case "J":
+                    return 11;
+                case "Q":
+                    return 12;
+                case "K":
+                    return 13;
+                case "A":
+                    return 14;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
